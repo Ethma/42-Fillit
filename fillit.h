0b03cb2a31012546fc8d8bb3915239c413b5e270
Abessira Mendy <Mendy@macbook-air-de-abessira.home>
2017-11-20 23:33:16 +0100

    20/11/2017 - 23:33 -> Auteur + header

---
+++ fillit.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Mendy <Mendy@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/20 23:25:21 by Mendy             #+#    #+#             */
+/*   Updated: 2017/11/20 23:27:36 by Mendy            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLIT_H
+# define FILLIT_H
+
+# include "libft.h"
+
+typedef struct	s_map
+{
+	int		size;
+	char	**tab;
+}				t_map;
+
+typedef struct	s_point
+{
+	int			x;
+	int			y;
+}				t_point;
