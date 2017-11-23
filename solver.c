2fcd14d5a07eb83afd4148720b6ddc12380b3344
Mendy ABESSIRA <mabessir@e3r10p15.42.fr>
2017-11-23 13:26:10 +0100

    23/11/2017 - 13:26

--- solver.c
+++ solver.c
@@ -6,7 +6,7 @@
 /*   By: mabessir <mabessir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/22 10:51:19 by mabessir          #+#    #+#             */
-/*   Updated: 2017/11/23 12:17:33 by mabessir         ###   ########.fr       */
+/*   Updated: 2017/11/23 13:19:45 by mabessir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,17 @@
 int		put_tetriminos()
 
 /* Function who test if a piece can be pose */
-int		check_tetrimino(t_map	*map, int x, int y)
+int		check_tetrimino(t_map	*map, t_tetri tetrimino, int x, int y)
 {
 	int i;
 	int j;
 
-	while (tetrimino->tetri[i] == '.' || tetrimino->tetri[i] == '\n')
-		i++;
-	if (map->tab[i][j] == '.')
+	i = 0;
+	while (ft_isalpha(tetrimino->tetri[i]) != 1)
 	{
-		map->tab[i][j] == tetrimino->tetri[i];
-		j++;
-	}
-	else if (map[i][j] == '\0')
+
 		i++;
-	else
-		j++;
+	}
 	return (1);
 }
 
@@ -51,7 +46,7 @@ int		place_all_tetri(t_tetri *tetrimino, t_map *map)
 		{
 			if (check_tetrimino(map, tetrimino, x, y) != 0)
 			{
-				put_tetriminos();    bnm
+				put_tetriminos();
 				if (place_all_tetri(tetrimino->next, map) != 1)
 					return (0);
 			}
--- tetri_parse.c
+++ tetri_parse.c
@@ -6,7 +6,7 @@
 /*   By: rpinoit <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/20 12:09:44 by rpinoit           #+#    #+#             */
-/*   Updated: 2017/11/21 13:12:00 by rpinoit          ###   ########.fr       */
+/*   Updated: 2017/11/22 15:25:45 by rpinoit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,13 +39,13 @@ static int		contact_check(char *buf)
 	return (0);
 }
 
-static int		tetri_check(char *buf)
+static int		tetri_check(char *buf, int max_tetri)
 {
 	int i;
-	int t_max;
+	int s_max;
 
 	i = 0;
-	t_max = 0;
+	s_max = 0;
 	while (i < 20)
 	{
 		if (i % 5 == 4 && buf[i] != '\n')
@@ -53,40 +53,41 @@ static int		tetri_check(char *buf)
 		if (i % 5 < 4 && buf[i] != '.' && buf[i] != '#')
 			return (0);
 		if (buf[i] == '#')
-			t_max++;
+			s_max++;
 		i++;
 	}
-	if (t_max != 4)
+	max_tetri++;
+	if (s_max != 4)
 		return (0);
 	if (contact_check(buf) == 0)
 		return (0);
-	return (1);
+	return (max_tetri);
 }
 
-int				tetri_parse(char **av)
+t_tetri			*tetri_parse(char **av)
 {
 	int		fd;
+	int		tetri_nb;
 	int		flag;
-	int		max_tetri;
 	char	buf[20];
+	t_tetri	*head;
 
 	flag = 0;
-	max_tetri = 0;
+	tetri_nb = 0;
+	head = NULL;
 	if ((fd = open(*av, O_RDONLY)) == -1)
-		return (0);
+		return (NULL);
 	while (read(fd, (void*)buf, 20) > 0)
 	{
 		flag = 0;
-		max_tetri++;
-		if (max_tetri > 26 || tetri_check(buf) == 0)
-			return (0);
+		if (tetri_nb > 25 || (tetri_nb = tetri_check(buf, tetri_nb)) == 0)
+			return (NULL);
+		head = ft_lst_tetri(buf, head, tetri_nb);
 		if (read(fd, (void*)buf, 1) == 1)
 			flag = 1;
 		if (flag && buf[0] != '\n')
-			return (0);
+			return (NULL);
 		ft_bzero(buf, 20);
 	}
-	if (flag)
-		return (0);
-	return (1);
+	return (flag == 1 ? NULL : head);
 }
