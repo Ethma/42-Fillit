3bf4e0ae28220ef724ee54cc04fa1809764b3e54
Mendy ABESSIRA <mabessir@e2r9p13.42.fr>
2017-11-28 14:20:49 +0100

    28/11/2017 - 14:20 -> upload

--- srcs/map.c
+++ srcs/map.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   map.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rpinoit <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: rpinoit <rpinoit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/23 13:17:01 by rpinoit           #+#    #+#             */
-/*   Updated: 2017/11/28 13:15:55 by rpinoit          ###   ########.fr       */
+/*   Updated: 2017/11/28 14:20:47 by mabessir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,6 +90,7 @@ void	ft_tetrin_map(t_tetri *head)
 		map->tab = NULL;
 		size++;
 		map = ft_create_map(size);
+		printf("%s\n", map->tab);
 	}
 	ft_print_map(map->tab);
 }
